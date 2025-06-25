3a1bea06203d8acf5d92ba05a44e8c00a6075bc3
Israela608 <israela608@gmail.com>
2025-06-25 21:08:43 +0100

    background image to onboarding

--- lib/modules/screens/onboarding_screen.dart
+++ lib/modules/screens/onboarding_screen.dart
@@ -3,6 +3,7 @@ import 'package:balanced_meal/core/helper/navigation.dart';
 import 'package:balanced_meal/core/utils/app_colors.dart';
 import 'package:balanced_meal/core/utils/app_styles.dart';
 import 'package:balanced_meal/core/utils/extensions.dart';
+import 'package:balanced_meal/core/utils/utils.dart';
 import 'package:balanced_meal/data/constants/strings.dart';
 import 'package:balanced_meal/modules/screens/enter_details_screen.dart';
 import 'package:flutter/material.dart';
@@ -16,42 +17,56 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          children: [
-            48.height,
-            Text(
-              Strings.appName,
-              style: abhayaLibreStyle(
-                fontSize: 48,
-                color: AppColor.white,
-                fontWeight: FontWeight.w800,
-              ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            bottom: 0,
+            left: -105.25.w,
+            //right: -105.25.w,
+            child: Image.asset(
+              'assets/images/onboarding.jpg',
+              fit: BoxFit.fitHeight,
             ),
-            Spacer(),
-            Text(
-              Strings.appDescription,
-              textAlign: TextAlign.center,
-              style: poppinsStyle(
-                fontSize: 20,
-                color: AppColor.textAsh,
-                fontWeight: FontWeight.w300,
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
+            child: Column(
+              children: [
+                48.height,
+                Text(
+                  Strings.appName,
+                  style: abhayaLibreStyle(
+                    fontSize: 48,
+                    color: AppColor.white,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  Strings.appDescription,
+                  textAlign: TextAlign.center,
+                  style: poppinsStyle(
+                    fontSize: 20,
+                    color: AppColor.textAsh,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                30.height,
+                WideButton(
+                  text: Strings.orderFood,
+                  onPressed: () {
+                    /* Navigation.gotoNamed(
+                      context,
+                      EnterDetailsScreen.route,
+                    );*/
+                  },
+                ),
+                43.height,
+              ],
             ),
-            30.height,
-            WideButton(
-              text: Strings.orderFood,
-              onPressed: () {
-                Navigation.gotoNamed(
-                  context,
-                  EnterDetailsScreen.route,
-                );
-              },
-            ),
-            43.height,
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
